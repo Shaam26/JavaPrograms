4e57ada69cd2258da87ea2c02054dd8d343b290e
Shameem Banu <Shameembanu561@gmail.com>
2025-07-30 12:51:10 +0530

    new files

---
+++ DataSizeRange.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class DataSizeRange
+{
+    public static void main(String args[])
+     {
+         
+       System.out.println("Byte Min: " + Byte.MIN_VALUE);
+       System.out.println("Byte Max: " + Byte.MAX_VALUE);
+       System.out.println(" Byte Bytes " +Byte.BYTES);
+     }
+}
---
+++ DataTypeDetail.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.swing.*;
+import java.awt.*;
+
+
+class DataTypeDetail
+
+{
+    public static void main(String args[])
+     {
+       int x=-5;
+         
+        
+
+       System.out.println(IntergerString(x));
+    
+         
+            }
+}
